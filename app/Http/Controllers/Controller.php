e167578f1dab6443ab654838c0b0387a6c0faabe
CodeSmiles <52099372+CodeSmiles@users.noreply.github.com>
2022-07-24 20:37:34 +0100

    Update on

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,37 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use illuminate\Support\Facades\Hash;
+use App\Models\User;
+use Illuminate\Http\Request;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    public function saveUser(Request $request)
+    {
+        // REGISTRATION CONTROLLER
+        $fields = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:8|confirmed',
+            'password_confirmation' => 'required|min:8'
+        ]);
+        $user = User::create([
+            'name' => $fields['name'],
+            'email' => $fields['email'],
+            'password' => bcrypt($fields['password'])
+        ]);
+
+        // generate a token for the user
+        $token = $user->createToken('authToken')->plainTextToken;
+
+        $response =[
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response, 201);
+    }
+    
 }
--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
-
+use App\Http\Controllers\Controller;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -19,12 +19,14 @@ return "<h1>Hello World<h1>";
 });
 
 Route::get("/all-post",[PostController::class,"index"]);
-Route::post("/upload",[PostController::class,"store"]);
+Route::post("/upload-post",[PostController::class,"store"]);
 Route::get("/post/{id}",[PostController::class,"show"]);
 Route::post("/post/{id}",[PostController::class,"update"]);
 Route::delete("/post/{id}",[PostController::class,"destroy"]);
 
-
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::get("/user",[Controller::class,"saveUser"]);
+});
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
