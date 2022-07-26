c9e928f2bd5d31725bac5851785758bafc1bf57c
CodeSmiles <52099372+CodeSmiles@users.noreply.github.com>
2022-07-26 12:13:42 +0100

    Update

--- app/Models/post.php
+++ app/Models/post.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class post extends Model
 {
     use HasFactory;
-    protected $fillable = ['title', 'slug', 'content'];
+    protected $fillable = ['title', 'slug', 'content',];
 
     public function user()
     {
